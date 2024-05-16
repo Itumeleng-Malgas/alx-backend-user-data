a52f00e3161e4d8eef68d9a16352c88108f07ad2
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-05-16 19:30:28 +0200

    Not PEP 8 compliant

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
-import re
+
 def filter_datum(fields, redaction, message, separator):
-    return re.sub(r'({})'.format('|'.join(fields)), redaction, message)
+    import re; return re.sub(r'({})'.format('|'.join(fields)), redaction, message)
