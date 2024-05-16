496dbcfd4093f29e96e3ccc87bfa3e0f1882291c
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-05-16 19:58:45 +0200

    Meet requirements

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """ Obfuscate messages """
 
-from typing import List
 import re
+from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """ returns the log message obfuscated """
-    return re.sub(r'({})'.format('|'.join(fields)), redaction, message)
+    """ Returns the log message obfuscated """
+    return re.sub(r'({}=).*?(?={})'.format('|'.join(map(re.escape, fields)),
+                  re.escape(separator)), r'\1' + redaction, message)
