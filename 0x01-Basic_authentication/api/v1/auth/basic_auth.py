626af6be5454d7cbf7f547ef5b525f726ad9cea7
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-05-30 22:13:41 +0200

    decode_base64_authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ Create a class BasicAuth that inherits from Auth
 """
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -17,3 +18,17 @@ class BasicAuth(Auth):
                       str) and authorization_header.startswith('Basic '):
             return authorization_header.split(' ', 1)[1]
         return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Decodes a Base64-encoded authorization header."""
+
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(
+                    base64_authorization_header.encode('utf-8'))
+            return decoded_bytes.decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
